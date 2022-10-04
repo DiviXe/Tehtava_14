10bada016cd6e9647890daee546871f72ec1ba6d
Leo Ahopalo <bgs208@myy-haaga.helia.fi>
2022-10-04 10:23:58 +0300

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import * as Speech from "expo-speech";
+import react, { useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+
+  const speak = () => {
+    Speech.speak(input);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(input) => setInput(input)}
+      />
+      <TouchableOpacity style={styles.button} onPress={speak}>
+        <Text>Press to hear Text</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    height: 30,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    backgroundColor: "gray",
+    borderWidth: 2,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "gray",
+    width: 150,
+    height: 34,
+    margin: 4,
   },
 });
